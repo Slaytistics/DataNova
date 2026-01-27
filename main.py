f5c9f5a34367f77cb5ca33af783d70b5bb6ca7ba
Sargam <sargamshabina06@gmail.com>
2026-01-27 21:16:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ from qna import ask_dataset_question
 
 app = FastAPI()
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
